3043432d31c8a909f60fb8398a56c2efc3fb6161
cys0329-dvlp <okkyuns0329@gmail.com>
2026-07-30 16:53:56 +0900

    initial commit

---
+++ srcDay1/Day01_Task01/Day01_Task01/main.c
@@ -0,0 +1,24 @@
+/*
+ * Day01_Task01.c
+ *
+ * Created: 2026-07-28 오후 11:29:01
+ * Author : ab201
+ */ 
+
+
+/*<
+정보> 
+PA0 ~ PA7 - LED
+
+*/ 
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
