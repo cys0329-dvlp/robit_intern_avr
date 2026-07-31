989650473684594524cbe42928840767b6e46586
cys0329-dvlp <okkyuns0329@gmail.com>
2026-08-01 02:49:17 +0900

    Update main.c

--- src/Day3/Day03_Task02/main.c
+++ src/Day3/Day03_Task02/main.c
@@ -9,7 +9,7 @@ void UART_transmit_string(char *str);
 
 int main(void)
 {
-	UBRR0L = 16;    // Baud Rate : 57600bps (16MHz)
+	UBRR0L = 16;    // Baud Rate : 57600bps
 	UBRR0H = 0;
 	UCSR0B = 0x18;  // 송신, 수신 기능 활성화 (RXEN0, TXEN0)
 	UCSR0C = 0x06;  // START 1비트 / DATA 8비트 / STOP 1비트
@@ -78,6 +78,11 @@ int main(void)
 			UART_transmit_string("7 LED on\r");
 			_delay_ms(100);
 		}
+		/*
+		예를 들어 1111 1011 인데 오른쪽으로 한칸 옮기면 0111 1101이 되기 때문에 왼쪽 오른쪽으로 움직일 때마다
+		양쪽에 1을 다시 덮어 씌워줘야함. 
+		동영상 촬영 방향에 따라 다르게 보일 수 있습니다.
+		*/
 		else if(input_data == '8')
 		{
 			PORTA = (PORTA >> 1)| 0b10000000;
@@ -86,11 +91,11 @@ int main(void)
 		}
 		else if(input_data == '9')
 		{
-			PORTA = (PORTA << 1)| 0b00000001;
+			PORTA = (PORTA << 1)| 0b00000001; 
 			UART_transmit_string("RIGHT\r");
 			_delay_ms(100);
 		}
-		else
+		else // 0~9 숫자 말고 다른 문자 혹은 범위를 초과하는 수를 넣었을 때 오류 처리
 		{
 			UART_transmit_string("Please Enter Numbers between 0 and 9\r"); 
 		}
@@ -100,21 +105,21 @@ int main(void)
 
 unsigned char Uart_Getch(void)
 {
-	while(!(UCSR0A & (1 << RXC0)));
-	return UDR0;
+	while(!(UCSR0A & (1 << RXC0))); // UCSR0A의 Bit7 활성화 -> 데이터를 입력 받을 준비가 됐다는 flag(Datasheet 189P 참고)
+	return UDR0; // 입력 buffer
 }
 
 void Uart_Putch(unsigned char PutData)
 {
-	while(!(UCSR0A & (1 << UDRE0)));
-	UDR0 = PutData;
+	while(!(UCSR0A & (1 << UDRE0))); // UCSR0A의 Bit5 활성화 -> 새로운 Data를 입력받을 준비가 됐다는 flag (Datasheet 189P 참고)
+	UDR0 = PutData; //받은 값을 입력 buffer에 넣음
 }
 
 void UART_transmit_string(char *str)
 {
-	while(*str != '\0')
+	while(*str != '\0') // \0이면 while 문 중지
 	{
-		Uart_Putch(*str);
+		Uart_Putch(*str); 
 		str++;
 	}
 }
